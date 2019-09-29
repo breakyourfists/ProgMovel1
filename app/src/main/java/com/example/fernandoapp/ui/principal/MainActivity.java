23e6b6e11b8283f1dfd2b05d593d444f59ba781a
breakyourfists <43384918+breakyourfists@users.noreply.github.com>
2019-09-29 14:07:00 -0300

    Funcionalidade de Abrir Mapa implantada.

--- app/src/main/java/com/example/fernandoapp/ui/principal/MainActivity.java
+++ app/src/main/java/com/example/fernandoapp/ui/principal/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.fernandoapp.R;
 import com.example.fernandoapp.data.model.Usuario;
+import com.example.fernandoapp.ui.login.LoginActivity;
+import com.example.fernandoapp.ui.mapa.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "Bem vindo "+usuario.getNome(), Toast.LENGTH_SHORT).show();
 
+
+    }
+
+    public void abrirMapaButton(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
